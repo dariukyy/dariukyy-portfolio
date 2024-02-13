88fbc40de68fb01e54144641fd6cdd998358d844
MacOS <macos@MacBook-Pro.lan>
2024-02-13 23:10:01 +0200

    add header

---
+++ src/ui/Header.tsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { device } from "../utils/breakpoints";
+import { useLocation } from "react-router-dom";
+
+const StyledHeader = styled(motion.div)`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+  margin-bottom: -13rem;
+  z-index: 999;
+
+  & h1 {
+    font-size: 3.6rem;
+    font-weight: 500;
+    letter-spacing: 1.1px;
+    text-align: center;
+    color: var(--color-brand-500);
+    margin-top: 4rem;
+    text-shadow: 3px 5px 9px rgba(0, 0, 0, 0.2);
+  }
+
+  @media ${device.mobile} {
+    font-size: 1.6rem;
+  }
+`;
+
+function Header() {
+  const location = useLocation();
+  const path = location.pathname;
+
+  const headerString = path.slice(1).charAt(0).toUpperCase() + path.slice(2);
+
+  return (
+    <StyledHeader
+      initial={{ y: -100, opacity: 1 }}
+      animate={{ y: 0, opacity: 1 }}
+    >
+      <h1>{headerString}</h1>
+    </StyledHeader>
+  );
+}
+
+export default Header;
