741a48889200852a23be2d26e6d3ea20baae18b5
MacOS <macos@MacBook-Pro.lan>
2024-02-25 15:33:30 +0200

    update skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,13 +5,13 @@ import { RiSupabaseFill } from "react-icons/ri";
 import {
   SiCssmodules,
   SiFirebase,
-  SiJest,
+  // SiJest,
   SiReactquery,
   SiReactrouter,
   SiRedux,
   SiStyledcomponents,
   SiTailwindcss,
-  SiTestinglibrary,
+  // SiTestinglibrary,
   SiTypescript,
 } from "react-icons/si";
 import { TbBrandFramerMotion } from "react-icons/tb";
@@ -36,13 +36,13 @@ export const skills: SkillsProps = [
   { skill: "Redux", icon: SiRedux, id: 6, color: "#764abc" },
   { skill: "React Query", icon: SiReactquery, id: 7, color: "#ff4154" },
   { skill: "React Router", icon: SiReactrouter, id: 8, color: "#ca4245" },
-  { skill: "Jest", icon: SiJest, id: 9, color: "#99425b" },
-  {
-    skill: "React Testing Library",
-    icon: SiTestinglibrary,
-    id: 10,
-    color: "#ef4444",
-  },
+  // { skill: "Jest", icon: SiJest, id: 9, color: "#99425b" },
+  // {
+  //   skill: "React Testing Library",
+  //   icon: SiTestinglibrary,
+  //   id: 10,
+  //   color: "#ef4444",
+  // },
 
   { skill: "Git", icon: FaGitAlt, id: 11, color: "#f34f29" },
   { skill: "Firebase", icon: SiFirebase, id: 12, color: "#ffca28" },
