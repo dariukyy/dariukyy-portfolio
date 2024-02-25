8e62d7e62b21fa5c601d141e87001093b67fbef5
MacOS <macos@MacBook-Pro.lan>
2024-02-25 12:14:15 +0200

    add skill

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -5,11 +5,13 @@ import { RiSupabaseFill } from "react-icons/ri";
 import {
   SiCssmodules,
   SiFirebase,
+  SiJest,
   SiReactquery,
   SiReactrouter,
   SiRedux,
   SiStyledcomponents,
   SiTailwindcss,
+  SiTestinglibrary,
   SiTypescript,
 } from "react-icons/si";
 import { TbBrandFramerMotion } from "react-icons/tb";
@@ -34,28 +36,36 @@ export const skills: SkillsProps = [
   { skill: "Redux", icon: SiRedux, id: 6, color: "#764abc" },
   { skill: "React Query", icon: SiReactquery, id: 7, color: "#ff4154" },
   { skill: "React Router", icon: SiReactrouter, id: 8, color: "#ca4245" },
-  { skill: "Git", icon: FaGitAlt, id: 9, color: "#f34f29" },
-  { skill: "Firebase", icon: SiFirebase, id: 10, color: "#ffca28" },
-  { skill: "Supabase", icon: RiSupabaseFill, id: 11, color: "#33b382" },
+  { skill: "Jest", icon: SiJest, id: 9, color: "#99425b" },
+  {
+    skill: "React Testing Library",
+    icon: SiTestinglibrary,
+    id: 10,
+    color: "#ef4444",
+  },
+
+  { skill: "Git", icon: FaGitAlt, id: 11, color: "#f34f29" },
+  { skill: "Firebase", icon: SiFirebase, id: 12, color: "#ffca28" },
+  { skill: "Supabase", icon: RiSupabaseFill, id: 13, color: "#33b382" },
   {
     skill: "Framer Motion",
     icon: TbBrandFramerMotion,
-    id: 12,
+    id: 14,
     color: "#ff00f7",
   },
-  { skill: "Tailwind CSS", icon: SiTailwindcss, id: 13, color: "#38b2ac" },
+  { skill: "Tailwind CSS", icon: SiTailwindcss, id: 15, color: "#38b2ac" },
   {
     skill: "CSS Modules",
     icon: SiCssmodules,
-    id: 14,
+    id: 16,
     fill: "var(--color-grey-800)",
     transition: "all 0.3s",
   },
   {
     skill: "Styled Components",
     icon: SiStyledcomponents,
-    id: 15,
+    id: 17,
     color: "#db7093",
   },
-  { skill: "Sass", icon: FaSass, id: 16, color: "#cc6699" },
+  { skill: "Sass", icon: FaSass, id: 18, color: "#cc6699" },
 ];
