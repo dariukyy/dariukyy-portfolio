f53d305d065b47ab83ab4efd0017ca1fe2644446
Darius <dpdarius1@gmail.com>
2024-05-27 01:13:01 +0300

    added ScrollToTop button, with functionality, to the projects page

---
+++ src/ui/ScrollToTop.tsx
@@ -0,0 +1,64 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { TiArrowSortedUp } from "react-icons/ti";
+import styled from "styled-components";
+import { device } from "../utils/breakpoints";
+
+const ScrollUpButton = styled(motion.button)`
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+  z-index: 1000;
+  background-color: var(--color-brand-500);
+  color: var(--color-white);
+  width: 6rem;
+  height: 6rem;
+  border-radius: 30%;
+  box-shadow: var(--shadow-md);
+  border: none;
+
+  @media ${device.mobile} {
+    bottom: 2rem;
+    right: 2rem;
+  }
+
+  & svg {
+    width: 4rem;
+    height: 4rem;
+    color: #dcddf2;
+  }
+`;
+
+function ScrollToTop() {
+  const [isMounted, setIsMounted] = useState(true);
+
+  function handleScrollToTop() {
+    const scrollContainer = document.querySelector(".ScrollComponent");
+    if (scrollContainer) {
+      scrollContainer.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    setIsMounted(false);
+  }
+  return (
+    <AnimatePresence>
+      {isMounted && (
+        <ScrollUpButton
+          onClick={handleScrollToTop}
+          initial={{ y: 100, opacity: 1 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          exit={{ y: 100, opacity: 0 }}
+        >
+          <TiArrowSortedUp />
+        </ScrollUpButton>
+      )}
+    </AnimatePresence>
+  );
+}
+
+export default ScrollToTop;
