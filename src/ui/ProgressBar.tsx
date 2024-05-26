f7e087c3ba3844997e597ea99ff0a124cc3e2719
Darius <dpdarius1@gmail.com>
2024-05-27 00:53:54 +0300

    added scroll progress bar functionality on projects page

---
+++ src/ui/ProgressBar.tsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+
+function ProgressBar({
+  setScrollPercentage,
+  scrollPercentage,
+}: {
+  setScrollPercentage: (scrollPercentage: number) => void;
+  scrollPercentage: number;
+}) {
+  useEffect(() => {
+    function handleScroll() {
+      const scrollContainer = document.querySelector(".ScrollComponent");
+      if (scrollContainer) {
+        const scrollPosition = scrollContainer.scrollTop;
+        const totalScrollHeight =
+          scrollContainer.scrollHeight - scrollContainer.clientHeight;
+        const scrollPercent = (scrollPosition / totalScrollHeight) * 100;
+        setScrollPercentage(scrollPercent);
+      }
+    }
+
+    const scrollContainer = document.querySelector(".ScrollComponent");
+    if (scrollContainer) {
+      scrollContainer.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (scrollContainer) {
+        scrollContainer.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      id="progress-container"
+      style={{
+        zIndex: 100,
+        height: "6px",
+        width: "100%",
+        backgroundColor: "transparent",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <div
+        className="progress-fill"
+        style={{
+          zIndex: 100,
+          height: "100%",
+          width: `${scrollPercentage}%`,
+          backgroundColor: "var(--color-brand-500)",
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default ProgressBar;
